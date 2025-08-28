dc80f8187557194b510b67a3ef2cc02899827b3d
DreamWorksCabinetry <mcelhaneyphilip@gmail.com>
2025-08-28 14:06:47 -0400

    Create Index.js

---
+++ Pages/Index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px", fontFamily: "Arial" }}>
+      <h1>Welcome to DreamWorks Cabinetry</h1>
+      <p>A true custom cabinet shop in Valley Head, Alabama.</p>
+      <p>Site is under construction 🚀</p>
+    </div>
+  );
+}
